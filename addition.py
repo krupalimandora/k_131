a01beee6ee9fd7656404a931fcd8d98e1ec125e5
krupalimandora <154410225+krupalimandora@users.noreply.github.com>
2023-12-20 12:58:38 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,4 +1,8 @@
-number1=input("first number")
-number2=input("second number")
-sum=float(number1)+(number2)
-print("The sum of {0} and {1} is {2}" .format(number1,number2, sum))
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
